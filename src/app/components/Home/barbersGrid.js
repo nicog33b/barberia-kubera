1a653162f20ca457671ed8cb993f057bd2105305
Nicolás García <88388887+nicog33b@users.noreply.github.com>
2024-04-18 22:45:48 -0300

    upgrade barbersgrid.js

--- src/app/components/Home/barbersGrid.js
+++ src/app/components/Home/barbersGrid.js
@@ -8,9 +8,9 @@ function BarbersGrid() {
   ];
 
   return (
-    <div className="flex flex-wrap justify-center items-center gap-6 p-8 bg-white">
+    <div className="flex flex-wrap justify-center items-center gap-6 p-8 bg-white w-full">
       {barbers.map(barber => (
-        <div key={barber.id} className="flex flex-col items-center bg-zinc-950 p-4 rounded-lg shadow-xl transition duration-300 ease-in-out hover:bg-yellow-600">
+        <div key={barber.id} className="flex flex-col items-center bg-zinc-950 p-4 rounded-lg shadow-xl transition duration-300 ease-in-out hover:bg-yellow-600 w-full md:w-6/12">
           <div className="w-32 h-32 md:w-40 md:h-40 xl:w-48 xl:h-48 rounded-full overflow-hidden border-4 border-gray-300 shadow-lg">
             <Image 
               src={barber.imageSrc} 
